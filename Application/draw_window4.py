963ac18a9b74093ec9c47db8eb9d5453c2b0987b
root <sp39427@zut.edu.pl>
2019-12-11 17:47:56 +0000

    Improvements for chat - text editing and displaying messages

--- Application/draw_window4.py
+++ Application/draw_window4.py
@@ -2,18 +2,20 @@ import curses
 import random
 import string
 # TODO: if window is too small, it crashes
-
+# tODO: implement thread with a stack that receives messages and prints them
 
 class ChatManager:
     def __init__(self):
         self.stdscr = curses.initscr()
-        self.prompt = '#'+''.join(random.choice(string.ascii_uppercase+string.digits) for i in range(4)) + " > "
+        self.nick   = ''.join(random.choice(string.ascii_uppercase+string.digits) for i in range(4))
+        self.prompt = lambda n: '#'+ str(n) + " > "
         self.curr_msg_row_nr = 1
         self.publ_row_nr     = 1
         self.height, self.width = self.stdscr.getmaxyx()
-        self.user_msg = None
-        self.all_msg  = None
+        self.user_msg_box = None
+        self.all_msg_box  = None
         self.current_user_prompt = ""
+        self.current_message = {"nick":self.nick, "msg":str()}
 
         # Standard startup. Probably don't need to change this
         # Clear and refresh the screen for a blank canvas
@@ -34,37 +36,40 @@ class ChatManager:
     #
 
     def get_user_message(self):
-        self.user_msg = self.initialize_window(self.height // 7, self.width, self.height - self.height // 7, 0, True)
-        current_message = ""
+        self.user_msg_box = self.initialize_window(self.height // 7, self.width, self.height - self.height // 7, 0, True)
+        self.current_message['msg'] = str()
 
         while True:
             k = self.stdscr.getch()
 
             if k in [curses.KEY_ENTER, ord('\n'), 10, 13]:
-                return current_message
+                return self.current_message
+
+            if k in [curses.KEY_BACKSPACE]:
+                self.current_message['msg'] = self.current_message['msg'][:-1]
 
             elif chr(k).isprintable():
-                current_message += chr(k)
+                self.current_message['msg'] += chr(k)
 
-            if len(current_message):
-                self.display_message(self.user_msg, current_message, self.curr_msg_row_nr)
+            if len(self.current_message['msg']):
+                self.display_message(self.user_msg_box, self.current_message.copy(), self.curr_msg_row_nr)
 
-            self.user_msg.refresh()
+            self.user_msg_box.refresh()
     #
 
     def main_window(self):
-        self.all_msg = self.initialize_window(self.height - self.height // 7, self.width, 0, 0)
+        self.all_msg_box = self.initialize_window(self.height - self.height // 7, self.width, 0, 0)
 
         while True:
             published = self.get_user_message()
-            self.publ_row_nr = self.display_message(self.all_msg, published, self.publ_row_nr, self.prompt)
-            self.all_msg.refresh()
+            self.publ_row_nr = self.display_message(self.all_msg_box, published, self.publ_row_nr, self.prompt)
+            self.all_msg_box.refresh()
     #
 
     def initialize_window(self, ht, wdh, ystart, xstart, is_user=False):
         handle = curses.newwin(ht, wdh, ystart, xstart)
         if is_user is True:
-            handle.addstr(1, 1, self.prompt)
+            handle.addstr(1, 1, self.prompt(self.nick))
         handle.box()
         handle.refresh()
         return handle
@@ -73,23 +78,35 @@ class ChatManager:
     def display_message(self, window, message, row_nr, usr_prompt=None):
         row_nr_temp = 0
         winh, winw  = window.getmaxyx()
-        winw = winw - len(self.prompt) - 2
+        winw = winw - len(self.prompt(self.nick)) - 2
         winh = winh - 2
 
-        if usr_prompt is not self.current_user_prompt and usr_prompt is not None:
-            window.addstr(row_nr, 1, usr_prompt)
+        # Heres the thing
+        # do not separate prompt and message, the prompt should be a part of it
+        # placed at the start.
+        # If entire message is longer than etc. then display last part of it
 
-        if len(message) > winw:
-            rows_msg = [message[i:i + winw] for i in range(0, len(message), winw)]
-            for row in rows_msg:
-                window.addstr(row_nr, len(self.prompt) + 1, row)
+        # if usr_prompt is not self.current_user_prompt and usr_prompt is not None:
+            # window.addstr(row_nr, 1, usr_prompt)
+
+        if row_nr >= winh:
+            if len(message['msg']) > winw:
+                window.clear(); window.box()
+                rows_msg = [message['msg'][i:i + winw] for i in range(0, len(message['msg']), winw)][-winh:]
+
+                window.addstr(row_nr, 1, self.prompt(message['nick']))
+                for row in rows_msg:
+                    window.addstr(row_nr, len(self.prompt(message['nick'])) + 1, row)
+                    row_nr += 1
+            else:
+                window.addstr(row_nr, 1, self.prompt(message['nick']))
+                window.addstr(row_nr, len(self.prompt(message['nick'])) + 1, message['msg'])
                 row_nr += 1
         else:
-            window.addstr(row_nr, len(self.prompt) + 1, message)
-            row_nr += 1
+            window.clear(); window.box()
 
-        if usr_prompt is not None:
-            self.current_user_prompt = usr_prompt
+        # if usr_prompt is not None:
+        #     self.current_user_prompt = usr_prompt
 
         return row_nr - row_nr_temp
     #
