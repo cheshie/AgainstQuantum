a90c6de72499413d1aec4df167f9e1b65a0264e2
PrzemyslawSamsel <sp39427@zut.edu.pl>
2019-12-16 18:18:10 +0000

    Testing FrodoKEM efficiency, created commands for that purpose in application

--- Application/__main__.py
+++ Application/__main__.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 from FrodoKEM.frodo640.api_frodo640 import FrodoAPI640
+from FrodoKEM.tests.test_kem import kem_test, kem_bench
 from Application.ConnectionManager import ConnectionManager
 import argparse
 import sys
@@ -20,52 +21,40 @@ import sys
 #-----------------------------------------
 
 
-def parse_arguments(args_parser = argparse.ArgumentParser()):
+def parse_arguments(args_parser = argparse.ArgumentParser(prog='appname', description='program description')):
     args_parser.add_argument('-p', help='connection port')
     args_parser.add_argument('-a', help='connection address')
     args_parser.add_argument('-i', help='start interactive mode. Commands available in the interactive mode:')
     args_parser.add_argument('-f', help='send file')
     args_parser.add_argument('-l', help='start server', action='store_true', default=False)
     args_parser.add_argument('-c', help='connect to a server', action='store_true', default=False)
-    args_parser.add_argument('--secure', help='starts secure connection')
-    args = args_parser.parse_args()
-
-    #args_parser.set_defaults()
+    args_parser.add_argument('--secure', help='starts secure connection', action='store_true', default=False)
+    args_parser.add_argument('--test', help='time benchmark of [operations]', action='store_true', default=False)
+    args_parser.add_argument('--operations', help='possible operations are:'
+                                                  'keyGeneration, encapsulation, decapsulation',
+                             default='keyGeneration')
+    args_parser.add_argument('--fails', help='test only if encapsulation matches decapsulation,'
+                                             'do not measure time', action='store_true', default=False)
+    args_parser.add_argument('--repeat', help='How many times to repeat time measurement', default=2)
+    args_parser.add_argument('--number', help='How many times execute specified operations', default=2)
 
     if len(sys.argv) == 1:
         args_parser.print_help()
 
-    return args
+    return args_parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    connectionManager = ConnectionManager()
 
     if args.c is True:
-        connectionManager.start_client()
+        ConnectionManager().start_client()
     if args.l is True:
-        connectionManager.start_server()
+        ConnectionManager().start_server()
 
-    exit()
-
-
-    # print("Hello in the console application. ")
-
-
-
-    # FrodoAPI640 = FrodoAPI640()
-    # crypto_kem_keypair = FrodoAPI640.crypto_kem_keypair_frodo640
-    # crypto_kem_enc = FrodoAPI640.crypto_kem_enc_frodo640
-    # crypto_kem_dec = FrodoAPI640.crypto_kem_dec_frodo640
-    #
-    # for x in range(2):
-    #     crypto_kem_keypair()
-    #     ss_encap = crypto_kem_enc()
-    #     ss_decap = crypto_kem_dec()
-    #
-    #     if ss_encap.all() == ss_decap.all() is not True:
-    #         print("ERROR!")
-    #     else:
-    #         print("Keys equal.")
-    #
+    if args.test is True:
+        if args.fails is True:
+            kem_test(FrodoAPI640.CRYPTO_ALGNAME, iterations=int(args.number))
+        else:
+            kem_bench(repetitions=(int(args.number)))
+#
--- FrodoKEM/tests/test_kem.py
+++ FrodoKEM/tests/test_kem.py
@@ -2,12 +2,13 @@ from sys import argv
 from timeit import Timer
 from numpy import array_equal
 from statistics import mean, stdev
+from threading import Thread
+import os
+from time import sleep
+from datetime import datetime
+from enum import Enum
 
-# Test parameters
-KEM_TEST_ITERATIONS = 2
-KEM_BENCH_SECONDS   = 1
-REPETITIONS         = 2
-
+"""
 # Print message if more than one arg is specified
 if len(argv) > 2:
     print("Program accepts only one argument.")
@@ -25,12 +26,13 @@ if len(argv) == 2:
 # As each of three has different set of parameters
 # Default option is FrodoKEM-640 - even if no args were supplied
 if len(argv) == 1 or argv[1] == "FrodoKEM-640":
-    from FrodoKEM.frodo640.api_frodo640 import FrodoAPI640
-    # Assign functions according to algorithm specified
-    FrodoAPI640 = FrodoAPI640()
-    crypto_kem_keypair = FrodoAPI640.crypto_kem_keypair_frodo640
-    crypto_kem_enc     = FrodoAPI640.crypto_kem_enc_frodo640
-    crypto_kem_dec     = FrodoAPI640.crypto_kem_dec_frodo640
+"""
+from FrodoKEM.frodo640.api_frodo640 import FrodoAPI640
+# Assign functions according to algorithm specified
+FrodoAPI640 = FrodoAPI640()
+crypto_kem_keypair = FrodoAPI640.crypto_kem_keypair_frodo640
+crypto_kem_enc     = FrodoAPI640.crypto_kem_enc_frodo640
+crypto_kem_dec     = FrodoAPI640.crypto_kem_dec_frodo640
 # For future compatibility
 # if argv[1] == "FrodoKEM-976":
 #     from api_frodo976 import *
@@ -38,50 +40,91 @@ if len(argv) == 1 or argv[1] == "FrodoKEM-640":
 #     from api_frodo1344 import *
 
 
-def kem_test(named_parameters, iterations):
-    print("=" * 125, "\n");
-    print(f"Testing correctness of key encapsulation mechanism (KEM), system {named_parameters},"
-          f" tests for {iterations} iterations\n")
-    print("=" * 125, "\n");
+def kem_test(named_parameters, iterations=2):
+    class Operations(Enum):
+        keyGeneration = 0
+        keyEncapsulation = 1
+        keyDecapsulation = 2
 
-    for x in range(iterations):
-        crypto_kem_keypair()
-        ss_encap = crypto_kem_enc()
-        ss_decap = crypto_kem_dec()
+    def tester(iterations):
+        global correct; correct = 0
+        global incorrect; incorrect = 0
+        global iteration_time; iteration_time = 0
+        global current_operation; current_operation = 0
+        # Add also percentage of incorrect results, or all results??
 
-        if array_equal(ss_encap, ss_decap) is not True:
-            print("ERROR!")
-            return False
-        else:
-            print("Keys equal.")
+        # ADD VECTOR WITH INCORRECT RESULTS!!!
+        start_time = datetime.now()
+        for x in range(iterations):
+            crypto_kem_keypair()
+            current_operation += 1
+            ss_encap = crypto_kem_enc()
+            current_operation += 1
+            ss_decap = crypto_kem_dec()
+            current_operation += 1
+            current_operation = current_operation % 3
+            if array_equal(ss_encap, ss_decap) is not True:
+                incorrect += 1
+            else:
+                correct   += 1
+            iteration_time = (datetime.now() - start_time).total_seconds()
+            start_time     = datetime.now()
+        sleep(1)
+    #
 
-    print("Tests PASSED. All sessions keys matched.\n")
+    test_thread = Thread(target=tester, args=(iterations,))
+    test_thread.start()
+    time_started = datetime.now()
+    time_finished = 0
+    while test_thread.isAlive():
+        _ = os.system("clear")
+
+        print("=" * 101, "\n")
+        print(f"Testing correctness of key encapsulation mechanism (KEM), system {named_parameters},"
+              f" tests for {iterations} iterations\n")
+        print("=" * 101, "\n")
+        print(f"Time started: {time_started}\n", end="")
+        if iteration_time > 0:
+            print(f"Estimated time: {int(iteration_time * (iterations - incorrect - correct))} seconds\n", end="")
+        print(f"Current operation: {str(Operations(current_operation))[len('Operations.'):]}\n", end="")
+        print(f"Current iteration: {incorrect+correct}\n", end="")
+        print(f"Correct results: {correct}\n", end="")
+        print(f"Incorrect results: {incorrect}\n", end="")
+        progress = (incorrect + correct) * 20 // iterations
+        print("["+"#" * progress + (20 - progress) * ' '+']',"("+str(progress / 20 * 100)+"%)") # <= change formula here??? It should not consider length of bar, only real results # MAKE IT INT
+        sleep(1)
+
+    # Thread synchronization
+    test_thread.join()
+    time_finished = datetime.now()
+
+    print("Tests finished.\n")
+    print("Time finished: ",time_finished)
+    print("Tests have taken ", round((time_finished - time_started).total_seconds(), 2), "seconds")
     return True
 #
 
 
-def kem_bench(seconds):
+# should not it be iterations 1? and repetitions 10 for ex, then it will execute that outer
+# loop 10 times and add 10 different times (otherwise average and best should be same?)
+def kem_bench(kem_test_iterations = 1, repetitions = 2):
     print("{:<20s} {:<20s} {:<20s} {:<20s} {:<20s}".format("Operation", "Total time (s)", "Average (s)", "Best (s)",
                                                            "Std.dev (s)"))
 
     titles = ["   Key generation: ", "KEM encapsulation: ", "KEM decapsulation: "]
     functions = [crypto_kem_keypair, crypto_kem_enc, crypto_kem_dec]
 
-    for fun,tl in zip(functions,titles):
-        res = Timer(lambda: fun(), 'gc.enable()').repeat(repeat=KEM_TEST_ITERATIONS, number=REPETITIONS)
-        print(tl+5*" "+"{:<20s} {:<20s} {:<20s} {:<20s}".format(str(round(sum(res), 3)), str(round(mean(res),3)),
-                                                          str(round(min(res),3)), str(round(stdev(res),3))))
+    for fun, tl in zip(functions, titles):
+        res = Timer(lambda: fun(), 'gc.enable()').repeat(repeat=repetitions, number=kem_test_iterations)
+        print(tl+5 * " " + "{:<20s} {:<20s} {:<20s} {:<20s}".format(str(round(sum(res), 3)), str(round(mean(res), 3)),
+                                                                    str(round(min(res), 3)), str(round(stdev(res), 3))))
 #
 
 
 def main():
-    OK = kem_test(FrodoAPI640.CRYPTO_ALGNAME, KEM_TEST_ITERATIONS)
+    OK = kem_test(FrodoAPI640.CRYPTO_ALGNAME)
 
     if OK is True:
-        kem_bench(KEM_BENCH_SECONDS)
+        kem_bench()
 
     exit(0)
-
-
-# main function execution
-main()
