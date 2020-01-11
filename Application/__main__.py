506774f5162c20124c23caa780f7e36905ed3336
Przemek <sp39427@zut.edu.pl>
2020-01-11 12:26:56 +0100

    Small change in Application cmd arguments

--- Application/__main__.py
+++ Application/__main__.py
@@ -7,23 +7,30 @@ import argparse
 import sys
 
 
-def parse_arguments(args_parser = argparse.ArgumentParser(prog='appname', description='program description')):
-    args_parser.add_argument('-p', help='connection port')
-    args_parser.add_argument('-a', help='connection address')
-    args_parser.add_argument('-i', help='start interactive mode. Commands available in the interactive mode:')
-    args_parser.add_argument('-f', help='send file')
-    args_parser.add_argument('-l', help='start server', action='store_true', default=False)
-    args_parser.add_argument('-c', help='connect to a server', action='store_true', default=False)
-    args_parser.add_argument('--secure', help='starts secure connection', action='store_true', default=False)
-    args_parser.add_argument('--mode', help='starts secure connection', default='text', choices=['visual','text']) # metavar=('bar', 'baz'),
-    args_parser.add_argument('--test', help='time benchmark of [operations]', action='store_true', default=False)
-    args_parser.add_argument('--ops', help='possible operations are: keyGeneration, encapsulation, decapsulation',
+def parse_arguments(args_parser = argparse.ArgumentParser(prog='Application', description='Chat and key exchange testing')):
+    clientserv = args_parser.add_argument_group('client-server basic options')
+    chat = args_parser.add_argument_group('chat options')
+    tests = args_parser.add_argument_group('testing key exchange')
+
+    clientserv.add_argument('-p', help='connection port')
+    clientserv.add_argument('-a', help='connection address')
+    # args_parser.add_argument('-i', help='start interactive mode. Commands available in the interactive mode:')
+    clientserv.add_argument('-l', help='start server', action='store_true', default=False)
+    clientserv.add_argument('--svconn', help='number of clients that can be connected to the server at the time')
+    clientserv.add_argument('-c', help='connect to a server', action='store_true', default=False)
+    clientserv.add_argument('-f', help='send file to the server - provide path to the file')
+    chat.add_argument('--clport', help="change port on which client will listen for server's response (default 9777)")
+    chat.add_argument('--nick', help='change default-random nick')
+    chat.add_argument('--secure', help='starts secure connection', action='store_true', default=False)
+    chat.add_argument('--mode', help='display mode', default='text', choices=['visual','text']) # metavar=('bar', 'baz'),
+    tests.add_argument('--test', help='time benchmark of [operations]. Default is testing of all operations', action='store_true', default=False)
+    tests.add_argument('--ops', help='possible operations are: keyGeneration, encapsulation, decapsulation',
                              default='keyGeneration')
-    args_parser.add_argument('--fails', help='test only if encapsulation matches decapsulation,'
-                                             'do not measure time', action='store_true', default=False)
-    args_parser.add_argument('--repeat', help='How many times to repeat time measurement', default=2)
-    args_parser.add_argument('--number', help='How many times execute specified operations', default=2)
-    args_parser.add_argument('--system', help='Choose FrodoKEM version\n (Frodo640, Frodo976, Frodo1344)',
+    tests.add_argument('--fails', help='test only if encapsulation matches decapsulation,'
+                                             'time benchmark is not the goal', action='store_true', default=False)
+    tests.add_argument('--repeat', help='How many times to repeat time measurement', default=2)
+    tests.add_argument('--number', help='How many times execute specified operations', default=2)
+    tests.add_argument('--system', help='Choose FrodoKEM version\n (Frodo640, Frodo976, Frodo1344)',
                              default='Frodo640')
 
     if len(sys.argv) == 1:
